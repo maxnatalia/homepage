2990bb54f356670a7cd1bc07d0cbe904328bfd84
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-05 10:56:07 +0100

    added interaction for modals

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 {
-  const welcome = () => {
-    console.log("Cześć!");
+  const buttonFridaVote = document.querySelector(".js-buttonFridaVote");
+  const buttonFelekVote = document.querySelector(".js-buttonFelekVote");
+  const buttonFrankVote = document.querySelector(".js-buttonFrankVote");
+  const textVote = document.querySelector(".js-textVote");
+  const overlay = document.querySelector(".js-overlay");
+  const modal = document.querySelector(".js-modal");
+  const closeButton = document.querySelector(".js-closeButton");
+  const buttonContact = document.querySelector(".js-buttonContact");
+
+  const openModal = () => {
+    overlay.classList.remove("modal__hidden");
+    modal.classList.remove("modal__hidden");
   }
 
-  const textVote = document.querySelector(".js-textVote");
+  const closeModal = () => {
+    overlay.classList.add("modal__hidden");
+    modal.classList.add("modal__hidden");
+  }
+
+  const closeKeyboardModal = () => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" && !modal.classList.contains("modal__hidden")) {
+        closeModal();
+      }
+    });
+  }
 
   const onChangedTekstFrida = () => {
+    openModal();
     textVote.innerHTML = ("Frida najczęściej mówi <i>Halo</i> i <i>Naprawdę???</i>");
   };
 
   const onChangedTekstFelek = () => {
+    openModal();
     textVote.innerHTML = ("Felek najczęściej zadaje pytanie <i>Co Wy to robicie?</i> często też mówi <i>Cichutkooo</i>");
   };
 
   const onChangedTekstFrank = () => {
+    openModal();
     textVote.innerHTML = ("Frank często pyta <i> Co chcesz? </i>");
   };
 
@@ -26,15 +50,13 @@
   };
 
   const init = () => {
-    const buttonFridaVote = document.querySelector(".js-buttonFridaVote");
-    const buttonFelekVote = document.querySelector(".js-buttonFelekVote");
-    const buttonFrankVote = document.querySelector(".js-buttonFrankVote");
+    closeButton.addEventListener("click", closeModal);
+    overlay.addEventListener("click", closeModal)
     buttonFridaVote.addEventListener("click", onChangedTekstFrida);
     buttonFelekVote.addEventListener("click", onChangedTekstFelek);
     buttonFrankVote.addEventListener("click", onChangedTekstFrank);
-    const buttonContact = document.querySelector(".js-buttonContact");
     buttonContact.addEventListener("click", toggleContact);
+    closeKeyboardModal();
   }
-  welcome();
   init();
 }
